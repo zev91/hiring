e91a4fa0982f26a3ada9b8dd50fcae8279fcf023
zev91 <460454298@qq.com>
2017-10-25 17:55:15 +0800

    add

--- src/js/index.js
+++ src/js/index.js
@@ -37,11 +37,80 @@
     });
     var $width = $(window).width();
     var $height = $(window).height()
-    if ($width / $height > 0.57) {
+
+    // var $rem = ($width / $height>0.597)?(0.597/($width / $height)*75):750
+    // $('html').css('font-size',$rem+'px')
+    if (0.65>$width / $height > 0.57) {
         $('.item1 .qt_slogan').css('margin-top', '3vw')
-        // $('.item1 .qr_code ').css('margin-top','1.4vw')
-        // $('.item1 .qt_logo ').css('margin-top','3.2vw')
+
     }
+    if ($width / $height > 0.65) {
+        $('.item1 .qt_slogan').css('margin-top', '3vw')
+        $('.item1 .qr_code ').css('margin-top','1.4vw')
+        $('.item1 .qt_logo ').css('top','3.2vw')
+        $('.item1 .qt_logo ').css('left','3.2vw')
+        $('.about .profile>p.align_left').css('margin','2.8vw 0')
+        $('.about .profile').css('padding-top','0')
+        $('.jobs .jobs_btn .btn ').css('margin-bottom','1.2vw')
+        $('.jobs .job_info p ').css('margin','1.2vw 0')
+        $('.jobs .eveirment_ct').css('height','103vw')
+    }
+
+    var startPosition, endPosition, moveLength;  
+    $('.imagec_ct').bind('touchstart', function(e){  
+    var touch = e.touches[0];  
+    startPosition = {  
+        x: touch.pageX 
+    }
+}) .bind('touchmove', function(e){  
+    var touch = e.touches[0];  
+    endPosition = {  
+        x: touch.pageX
+    };  
+    moveLength = endPosition.x - startPosition.x;
+});
+// 切换
+    var numbEve = parseInt($('.eveirment_ct').width());
+    var marginLeft = $('.imagec_ct').css('margin-left');
+    var numbLeft =  parseInt(marginLeft);
+    var index = 0;
+    setBubble(index)
+$('.imagec_ct').swipeLeft(function(){
+    if(Math.abs(moveLength)>40){
+        if(numbLeft == -numbEve*2){
+            numbLeft=0
+            index = 0
+        }else{
+            numbLeft -= numbEve
+            index +=1;
+   
+        }
+        $('.imagec_ct').css('margin-left',numbLeft+'px');
+        setBubble(index)
+       
+    }
+})
+$('.imagec_ct').swipeRight(function(){
+    if(Math.abs(moveLength)>40){
+        if(numbLeft == 0){
+            numbLeft = -numbEve*2
+            index = 2
+    
+        }else{
+            numbLeft += numbEve;
+            index  -=1;
+          
+        }
+        $('.imagec_ct').css('margin-left',numbLeft+'px');
+        setBubble(index)
+        
+    }
+})
+function setBubble(index){
+    var lis = $('.bubble_ct .bubble');
+    lis.removeClass('active');
+    $(lis[index]).addClass('active')
+}
     $('body').on('tap', '.skip_to', function (e) {
         e.preventDefault;
         
